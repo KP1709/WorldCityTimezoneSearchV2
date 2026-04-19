d4f1b8985f340ec7bf336d9dce41b84cde59bc12
Kareena Patel <kareenapat17@gmail.com>
2026-04-19 10:50:35 +0100

    Adjust styling to modal so uniform

--- src/components/multipleSearchResultsModal.tsx
+++ src/components/multipleSearchResultsModal.tsx
@@ -4,14 +4,23 @@ import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { useSearchCity } from "@/hooks/useSearchCity";
 import { supabase } from "@/hooks/getCityListData";
 import type { CitiesTypeGrouped } from "@/types";
+import { FieldLabel, Field, FieldContent, FieldTitle } from "@/components/ui/field";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { useEffect, useState } from "react";
 
 const MultipleSearchResultsModal = () => {
     const { hasManyResults, setHasManyResults, cityName, setSelectedCity, setHasMultipleCities, setSelectedCityGrouped } = useMapStore();
     const { results } = useSearchCity({ debouncedQuery: cityName, searchExactCity: true });
+    const [countrySelected, setCountrySelected] = useState<string>("");
 
     const selectItem = async (item: CitiesTypeGrouped) => {
         if (item.region.length === 1) {
-            const { data, error } = await supabase.rpc('get_selected_city', { city_selected: item.ascii_name, region_selected: item.region[0], country_selected: item.country_name_en });
+            const { data, error } = await supabase.rpc('get_selected_city',
+                {
+                    city_selected: item.ascii_name,
+                    region_selected: item.region[0],
+                    country_selected: item.country_name_en
+                });
 
             if (!error) {
                 setSelectedCity({
@@ -44,6 +53,11 @@ const MultipleSearchResultsModal = () => {
         selectItem({ ascii_name, country_name_en, region, geoname_id });
     };
 
+    useEffect(() => {
+        if (countrySelected === "") return;
+        handleResultsSelect(results.find((item) => item.country_name_en === countrySelected) as CitiesTypeGrouped);
+    }, [countrySelected]);
+
     return (
         <Dialog open={hasManyResults} onOpenChange={() => setHasManyResults(false)}>
             <DialogContent className="border-sidebar-primary">
@@ -53,17 +67,24 @@ const MultipleSearchResultsModal = () => {
                 <DialogDescription>Select a country</DialogDescription>
                 <ScrollArea className="h-fit max-h-[50vh] overflow-y-auto p-1.5">
                     <ScrollBar orientation="vertical" className="bg-sidebar-primary" />
-                    <ul>
-                        {results?.map((test) => {
+                    <RadioGroup
+                        className='grid sm:grid-cols-2 md:grid-cols-3'
+                        defaultValue=""
+                        onValueChange={(value) => { setCountrySelected(value); setHasManyResults(false); }}
+                    >
+                        {results.map(({ country_name_en, geoname_id }) => {
                             return (
-                                <li className='cursor-pointer rounded-md px-2 py-1 hover:bg-muted'
-                                    key={test.geoname_id}
-                                    onClick={() => { handleResultsSelect(test); setHasManyResults(false); }} >
-                                    {test.country_name_en}
-                                </li>
+                                <FieldLabel className="border-sidebar-primary hover:border-sidebar-primary-foreground cursor-pointer" key={geoname_id} htmlFor={country_name_en} >
+                                    <Field orientation="horizontal">
+                                        <FieldContent>
+                                            <FieldTitle>{country_name_en}</FieldTitle>
+                                        </FieldContent>
+                                        <RadioGroupItem className='cursor-pointer border-sidebar-primary' value={country_name_en} id={country_name_en} />
+                                    </Field>
+                                </FieldLabel>
                             );
                         })}
-                    </ul>
+                    </RadioGroup>
                 </ScrollArea>
             </DialogContent>
         </Dialog>
